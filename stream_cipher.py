3a6a8dd59d98771acc68c5645f5baec6a3e43a98
Brian <brineryte@gmail.com>
2021-03-04 17:05:21 -0500

    remove stuff

--- stream_cipher.py
+++
@@ -1,4 +0,0 @@
-# python has the walrus operator and list comprehensions so it will always win!
-print(y := [x for x in range(11) if x % 2 == 0])
-print(sorted(y + [x for x in range(10) if x % 2 != 0]))
-print(S := [2 * x for x in range(100) if x ** 2 > 500])
